bd4d2788c9c209a40be3b9449d3477b9f6398217
javasavvy <javasavvydev@gmail.com>
2015-08-26 23:42:04 -0700

    Trying to get 3rd party jQuery to work with dojo amd

--- src/main/java/com/javasavvy/beer/controller/IndexController.java
+++ src/main/java/com/javasavvy/beer/controller/IndexController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/thyme")
     public String index() {
         System.out.println("index controller");
         return "indexthyme";
